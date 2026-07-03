1b031bb7461fc27566838a7ee3c4c1109a76bd5b
PanayiotisLouca <79509801+PanayiotisLouca@users.noreply.github.com>
2026-07-03 11:56:21 +0100

    Add files via upload

---
+++ 1.Phage_Heritability/1.3.Individual_phage_heritability_SCRIPT_CREATE.R
@@ -0,0 +1,231 @@
+## Author: 
+# Panayiotis Louca 
+
+## Clear environment 
+    rm(list = ls()) 
+
+## Set seed 
+    set.seed(1234)
+      
+## load up packages: 
+
+    ### core 
+    library(tidyverse)
+    
+    # mets for heritability 
+    library(mets)
+    library(lava)
+
+# -------------------------------------------------------------------------- # 
+  
+    # ************************* # 
+    #   IMPORT & PREP DATA   ---- 
+    # ************************* # 
+   
+      ##   Dataset ---- 
+    
+    # HPC 
+    df <- read_rds(file.path("/scratch/users/k2480753/resistome/data/Revised_phages_microbes_metabs_DATASET.rds")) %>% 
+      as.data.frame() %>%
+      filter(phage_alpha_div_taxonomic_richness != 0)
+    
+    # vectorise column names 
+    phage_columns <- grep("^phage_[0-9]+", names(df), value = TRUE)
+
+# -------------------------------------------------------------------------- #  
+
+# create new dataset with just necessary variables 
+df_analyse <- df %>%
+  select(
+    iid,
+    all_of(covar_columns),
+    fid,
+    gm_zygosity,
+    all_of(phage_columns)
+  )
+
+# -------------------------------------------------------------------------- #  
+
+    ####   RUN LOOP OF SEM ON vOTUs  ---- 
+
+# split data into phenotype data and outcome data 
+
+    # phenotype data 
+phen <- data.frame(
+  fid = df_analyse$fid,
+  iid = df_analyse$iid,
+  zygosity = df_analyse$gm_zygosity,
+  age = df_analyse$gm_age,
+  bmi = df_analyse$gm_BMI,
+  sex = df_analyse$gm_sex
+)
+
+    # transposed outcome data 
+dat <- t(data.frame( 
+  df_analyse[ ,phage_columns]))
+
+# assign colnames of transposed outcome data as family id's 
+colnames(dat) <- df_analyse$fid
+
+dat <- as.data.frame(dat)
+
+# -------------------------------------------------------------------------- #  
+
+id <- phen$fid
+
+age <- phen$age
+
+zyg <- factor(phen$zygosity)
+
+bmi <- phen$bmi
+
+sex <- phen$sex
+
+################################################ Data input ###################################################
+
+res<-{}
+cor_sat <- {}
+model_tab <-{}
+
+for (i in 1:nrow(dat)) {
+  
+  methy<-	as.numeric(dat[i, ])
+  
+  data<-	data.frame(methy, zyg, id, age, bmi)
+  
+####################################### Assign formula and run ############################################# 
+  
+  formula <- as.formula("methy ~ age + bmi + sex")
+  
+  ace <- summary(twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ace"))
+  ae <- summary(twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ae"))
+  ce <- summary(twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ce"))
+  
+  e <- summary(twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "e"))
+  
+  sat <- summary(twinlm (formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "sat"))
+  
+############################### P value for models ##############################################
+  
+  ace2 <- twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ace")
+  ae2 <- twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ae")
+  ce2 <- twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "ce")
+  
+  e2 <- twinlm(formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "e")
+  
+  sat2<- twinlm (formula, id = "id", DZ = "DZ", zyg = "zyg", data = data, missing = FALSE, type = "sat")
+  
+##################################### Compare models ##################################################### 
+  
+  comparison_model<- sat2
+  
+  pval_ACE<- compare(ace2,sat2)
+  Rpval_ACE<- pval_ACE$p.value
+  
+  
+  pval_AE<- compare(ae2,comparison_model)
+  Rpval_AE<- pval_AE$p.value
+  
+  pval_CE<- compare(ce2,comparison_model)
+  Rpval_CE<- pval_CE$p.value
+  
+  pval_E<- compare(e2,comparison_model)
+  Rpval_E<- pval_E$p.value
+  
+#################################   Generate comparison metrics    ###########################################
+
+  ### Saturated model ### 
+  sat_2LL<- -2*logLik(sat2)
+  sat_AIC<- sat$AIC 
+  sat_df<- attr(logLik(sat2), "df")
+  
+  ### ACE ### 
+  ACE_2LL<- -2*logLik(ace2)
+  ACE_AIC<- ace$AIC 
+  ACE_df<- attr(logLik(ace2), "df")
+  ACE_DEL_2LL<- ACE_2LL - sat_2LL
+  ACE_DEL_df<- pval_ACE$parameter[1]
+  
+  ACE_CHI<- pval_ACE$statistic[1]
+  
+  ### AE ### 
+  AE_2LL<- -2*logLik(ae2)
+  AE_AIC<- ae$AIC 
+  AE_df<- attr(logLik(ae2), "df")
+  AE_DEL_2LL<- AE_2LL - sat_2LL
+  AE_DEL_df<- pval_AE$parameter[1]
+  AE_CHI<- pval_AE$statistic[1]
+  
+  ### CE ### 
+  CE_2LL <- -2*logLik(ce2)
+  CE_AIC <- ce$AIC
+  CE_df <- attr(logLik(ce2),"df")
+  CE_DEL_2LL <- CE_2LL - sat_2LL
+  CE_DEL_df <- pval_CE$parameter[1]
+  CE_CHI <- pval_CE$statistic[1]
+  
+  ### E ### 
+  E_2LL<- -2*logLik(e2)
+  E_AIC<- e$AIC 
+  E_df<- attr(logLik(e2), "df")
+  E_DEL_2LL<- E_2LL - sat_2LL
+  E_DEL_df<- pval_E$parameter[1]
+  E_CHI<- pval_E$statistic[1]
+  
+################################# Generate and format result table ###########################################
+  
+  res1<- rbind(
+    c(row.names(dat)[i],"ACE",ace$acde[1,],ace$acde[2,],"-","-","-",ace$acde[3,],ace$corMZ,ace$corDZ,ace$zyg[1],ace$zyg[2],"ACE",ACE_2LL,ACE_df,ACE_AIC,ACE_DEL_2LL,ACE_DEL_df,ACE_CHI,Rpval_ACE),
+    c(row.names(dat)[i],"AE", ae$acde[1,],"-","-","-","-","-","-",ae$acde[2,],ae$corMZ,ae$corDZ,ae$zyg[1],ae$zyg[2],"AE",AE_2LL,AE_df,AE_AIC,AE_DEL_2LL,AE_DEL_df,AE_CHI,Rpval_AE),
+    c(row.names(dat)[i],"E", "-","-","-","-","-","-","-","-","-",e$acde[1,],e$corMZ,e$corDZ,e$zyg[1],e$zyg[2],"E",E_2LL,E_df,E_AIC,E_DEL_2LL,E_DEL_df,E_CHI,Rpval_E),
+    c(row.names(dat)[i],"CE", "-","-","-",ce$acde[1,],"-","-","-",ce$acde[2,],ce$corMZ,ce$corDZ,ce$zyg[1],ce$zyg[2],"CE",CE_2LL,CE_df,CE_AIC,CE_DEL_2LL,CE_DEL_df,CE_CHI,Rpval_CE)
+  )
+  
+  res<- rbind(res, res1)
+  
+  cor_sat1<- rbind(c(sat$corMZ,sat$corDZ))
+  
+  cor_sat<- rbind(cor_sat,c(row.names(dat)[i],cor_sat1))
+  
+  mod_tab1 <- rbind(
+    c(row.names(dat)[i],"saturated",sat_2LL,sat_df,sat_AIC,"-","-","-","-"),
+    c (row.names(dat)[i],"ACE",ACE_2LL,ACE_df,ACE_AIC,ACE_DEL_2LL,ACE_DEL_df,ACE_CHI,Rpval_ACE),
+    c(row.names(dat)[i],"AE",AE_2LL,AE_df,AE_AIC,AE_DEL_2LL,AE_DEL_df,AE_CHI,Rpval_AE),
+    c(row.names(dat)[i],"E",E_2LL,E_df,E_AIC,E_DEL_2LL,E_DEL_df,E_CHI,Rpval_E)
+  )
+  
+  model_tab<-rbind(model_tab,mod_tab1)
+  
+}
+
+########################################## Output result table ###############################################
+
+head <- c("Trait",
+          "Model","A","A_2.50%","A_97.50%","C","C_2.50","C_97.50%","D","D_2.50","D_97.50%","E","E_2.50%","E_97.50%","Corr_MZ","Corr_MZ_2.50%","Corr_MZ_97.50%","Corr_DZ","Corr_DZ_2.50%","Corr_DZ_97.50%","MZ:pairs/singletons","DZ:pairs/singletons","Model","-2LL","df","AIC","D-2LL","D-df","chi","Pval")
+resF <- rbind(head,res)
+
+head_sat <- c("Trait",
+              "Corr_MZ","Corr_MZ_2.50%","Corr_MZ_97.50%",
+              "Corr_DZ","Corr_DZ_2.50%","Corr_DZ_97.50%")
+res_sat <- rbind(head_sat,cor_sat)
+
+
+head_mod <- c("Trait",
+              "Model","-2LL","df","AIC","D-2LL","D-df","chi","Pval")
+res_mod <- rbind(head_mod,model_tab)
+
+########################################## save result table ###############################################
+
+write.table(resF,
+            file = file.path("/scratch/users/k2480753/resistome/Individual_phage_heritability/Output_heritibility_viral_contigs.txt"),
+            col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+
+write.table(res_sat, 
+            file = file.path("/scratch/users/k2480753/resistome/Individual_phage_heritability/Output_heritibility_viral_contigs_CORR_SAT.txt"),
+            col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+
+write.table(res_mod, 
+            file =  file.path("/scratch/users/k2480753/resistome/Individual_phage_heritability/Output_heritibility_viral_contigs_MOD.txt"),
+            col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+
+# -------------------------------------------------------------------------- #  
